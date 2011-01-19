af397be00bf6f4ce43d35fa5182407226f1e4f3a
Jorge <jagbolanos@jagbolanos-laptop.(none)>
2011-01-19 06:57:37 -0600

    primeros

---
+++ cadenas/clasestring.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string cadena1 = "Hola";
+    string cadena2 = "Mundo";
+    
+    
+    cout << cadena1.length() << endl;
+    cout << cadena1 + " " + cadena2 << endl;
+    
+    return 0;
+}
